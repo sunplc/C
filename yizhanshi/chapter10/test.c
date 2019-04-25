256deeb7670daa7f352d5b54f6c358759164a234
sunplc <342274855@qq.com>
2019-04-25 23:44:58 +0800

    .

--- yizhanshi/chapter10/test.c
+++ yizhanshi/chapter10/test.c
@@ -2,10 +2,42 @@
 
 int add_range(int low, int high)
 {
-	int i = 0, sum = 0;
+	int i, sum;
 	for (i = low; i <= high; i++)
 		sum = sum + i;
 	return sum;
+}
+
+int fa(int x)
+{
+    int a = 10;
+    int b = 13;
+    printf("fa %p, a=%p, b=%p\n", &x, &a, &b);
+    x += x;
+    x += a + b;
+    return x;
+}
+
+int fb(int x)
+{
+    printf("fb %p\n", &x);
+    x = fa(x);
+    x *= x;
+    return x;
+}
+
+int fc(int x)
+{
+    printf("fc %p\n", &x);
+    x *= 2;
+    return x;
+}
+
+void foo(void)
+{
+    int i;
+    printf("i = %d\n", i);
+    i = 444;
 }
 
 int main(void)
@@ -14,5 +46,14 @@ int main(void)
 	result[0] = add_range(1, 10);
 	result[1] = add_range(1, 100);
 	printf("result[0] = %d\nresult[1] = %d\n", result[0], result[1]);
+
+    fa(1);
+    fb(2);
+    fa(3);
+    fc(4);
+
+    foo();
+    printf("hello\n");
+    foo();
 	return 0;
 }
