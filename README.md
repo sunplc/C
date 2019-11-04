0250cfe0d916cc6c501e8a181a0b229e155a3d0b
sunplc <342274855@qq.com>
2019-11-05 01:55:11 +0800

    update README.md

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 * 目录 krc： 书籍《The C Programming language, 2nd Edition》习题练习
 * 目录 yizhanshi：  书籍《Linux C编程一站式学习》习题练习
 * 目录 thinkos：  书籍《Think OS , A Brief Introduction to Operating Systems, Version 0.8.1》习题练习
+* 目录 pgu：  书籍《Programming from ground up》测试代码和练习
 
 ### 开发环境：
-* vim gcc gdb
+* Linux vim gcc
---
+++ thinkos/semaphores/utils.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "utils.h"
+
+void perror_exit(char *str)
+{
+    perror(str);
+    exit(-1);
+}
+
+void *check_malloc(long size)
+{
+    void * ptr = malloc(size);
+    if (ptr == NULL) {
+        perror_exit("malloc failed!");
+    }
+
+    return ptr;
+}
+
+Mutex *make_mutex()
+{
+    Mutex *mutex = (Mutex *) check_malloc(sizeof(Mutex));
+    if (pthread_mutex_init(mutex, NULL) != 0) {
+        perror_exit("pthread_mutex_init failed");
+    }
+    return mutex;
+}
+
+void mutex_lock(Mutex *mutex)
+{
+    if (pthread_mutex_lock(mutex) != 0) {
+        perror_exit("pthread_mutex_lock failed");
+    }
+}
+
+void mutex_unlock(Mutex *mutex)
+{
+    if (pthread_mutex_unlock(mutex) != 0) {
+        perror_exit("pthread_mutex_unlock failed");
+    }
+}
+
+Cond *make_cond()
+{
+    Cond *cond = (Cond *) check_malloc(sizeof(Cond));
+    if (pthread_cond_init(cond, NULL) != 0) {
+        perror_exit("pthread_cond_init failed");
+    }
+    return cond;
+}
+
+void cond_wait(Cond *cond, Mutex *mutex)
+{
+    if (pthread_cond_wait(cond, mutex) != 0) {
+        perror_exit("pthread_cond_wait failed");
+    }
+}
+
+void cond_signal(Cond *cond)
+{
+    if (pthread_cond_signal(cond) != 0) {
+        perror_exit("pthread_cond_signal failed");
+    }
+}
+
+pthread_t *make_thread(void *(*entry)(void *), void *arg)
+{
+    pthread_t *tid = (pthread_t *)check_malloc(sizeof(pthread_t));
+    if (pthread_create(tid, NULL, entry, arg) != 0) {
+        perror_exit("make_thread failed");
+    }
+
+    return tid;
+}
+
+void join_thread(pthread_t *tid)
+{
+    if (pthread_join(*tid, NULL) != 0) {
+        perror_exit("pthread_join failed");
+    }
+}
+
+void exit_thread()
+{
+    pthread_exit(NULL);
+}
