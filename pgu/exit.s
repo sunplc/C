4a666a1e741f9daf7ae6ba5d145dff1dd73793bb
sunplc <342274855@qq.com>
2019-11-06 00:56:10 +0800

    .

--- pgu/exit.s
+++ pgu/exit.s
@@ -7,4 +7,7 @@
 _start:
     movl $1, %eax   # system call number
     movl $-1, %ebx  # exit status code
+
+    # interrupt instruction, number 0x80 interrupt instruction
+    # is system call
     int $0x80
