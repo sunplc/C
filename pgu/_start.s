57d348438d15b34fe046f2aacb529af953b7c35e
sunplc <342274855@qq.com>
2019-11-21 18:27:54 +0800

    完成toupper练习

--- pgu/_start.s
+++ pgu/_start.s
@@ -1,13 +1,14 @@
-
-# 使用 objdump -S 查看生成的object文件，
-# 观察 _strart 和 $_start 的区别
+# 32位版本，可以在 x86_64 上正常执行
 
 .section .text
 .globl _start
 
 _start:
 
+    # 使用 objdump -S 分别查看生成的object文件，
+    # 观察其中 _start 和 $_start 的区别
     movl _start, %ebx
     # movl $_start, %ebx
+
     movl $1, %eax
     int $0x80
