b415132b96642214cb8e14b92b7cade09bb3f41a
sunplc <342274855@qq.com>
2020-04-09 13:52:55 +0800

    .

--- clear.sh
+++ clear.sh
@@ -5,3 +5,4 @@ find . -type f -name "*.out" -o -name "*.o"  | xargs rm -vf
 
 # 删除没有扩展名的文件，排除.开头的隐藏文件和目录，排除 Makefile 文件
 find . -type f ! -name  "*.*"  ! -name "*Makefile*"  -not -path '*/\.*' | xargs rm -vf
+
