32dce2876d11be00b2cb38e84c6b757641782d0a
sunplc <342274855@qq.com>
2020-07-06 17:31:43 +0800

    chapter3

---
+++ csapp2e/chapter3/code.s
@@ -0,0 +1,40 @@
+	.file	"code.c"
+	.intel_syntax noprefix
+	.text
+	.globl	accum
+	.bss
+	.align 4
+	.type	accum, @object
+	.size	accum, 4
+accum:
+	.zero	4
+	.text
+	.globl	sum
+	.type	sum, @function
+sum:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	DWORD PTR -20[rbp], edi
+	mov	DWORD PTR -24[rbp], esi
+	mov	edx, DWORD PTR -20[rbp]
+	mov	eax, DWORD PTR -24[rbp]
+	add	eax, edx
+	mov	DWORD PTR -4[rbp], eax
+	mov	edx, DWORD PTR accum[rip]
+	mov	eax, DWORD PTR -4[rbp]
+	add	eax, edx
+	mov	DWORD PTR accum[rip], eax
+	mov	eax, DWORD PTR -4[rbp]
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sum, .-sum
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
