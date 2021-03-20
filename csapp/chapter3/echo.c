53d7a21dde60a3a7b574756c3f449a8163d0ce73
sunplc <342274855@qq.com>
2021-03-21 00:05:26 +0800

    .

---
+++ csapp/chapter3/echo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char *gets(char *s) {
+    int c;
+    char *dest = s;
+
+    while ((c = getchar()) != '\n' && c != EOF) {
+        *dest++ = c;
+    }
+    // No characters read
+    if (c == EOF && dest == s)
+        return NULL;
+    *dest++ = '\0';
+    return s;
+}
+
+void echo()
+{
+    char buf[8];
+    gets(buf);
+    puts(buf);
+}
+
+int main()
+{
+    echo();
+}
