70b89d6a2fb5c0c3ec35289867b21e2a3c523951
sunplc <342274855@qq.com>
2021-04-09 20:24:05 +0800

    complete Homework problem 3.71

--- csapp/chapter3/3.71.c
+++ csapp/chapter3/3.71.c
@@ -1,3 +1,14 @@
+/*
+
+3.71 ◆
+Write a function good_echo that reads a line from standard input and writes it to
+standard output. Your implementation should work for an input line of arbitrary
+length. You may use the library function fgets, but you must make sure your
+function works correctly even when the input line requires more space than you
+have allocated for your buffer. Your code should also check for error conditions
+and return when one is encountered.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
