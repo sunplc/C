ef31e51e11310aa5a44cabe87dd697f6c3c758ba
sunplc <342274855@qq.com>
2022-08-18 18:17:35 +0800

    .

--- test/functional/CPS.c
+++ test/functional/CPS.c
@@ -7,9 +7,7 @@ function fact(n, ret) {
     if (n == 0)
         ret(1);
     else
-        fact(n - 1, function(t0) {
-            ret(n * t0);
-        });
+        fact(n - 1, function(t0) { ret(n * t0); } );
 }
 
 fact(10, function (n) {console.log(n);});
